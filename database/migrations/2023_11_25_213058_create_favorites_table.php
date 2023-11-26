90a9472c85ad8da1edef8e3f22046bbca36c8612
khalil <hydragamingnew@gmail.com>
2023-11-26 09:35:10 +0300

    first commit

--- database/migrations/2023_11_25_213058_create_favorites_table.php
+++ database/migrations/2023_11_25_213058_create_favorites_table.php
@@ -4,6 +4,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Medicine;
+
+
 
 class CreateFavoritesTable extends Migration
 {
@@ -12,13 +16,14 @@ class CreateFavoritesTable extends Migration
      */
     public function up()
     {
+        if(!Schema::hasTable('favorites')){
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('pharmacy_id')->constrained()->onDelete('cascade');
-            $table->foreignId('medicine_id')->constrained()->onDelete('cascade');
+            $table->foreign('pharmacy_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('medicine_id')->references('id')->on('medicines')->onDelete('cascade');
             $table->timestamps();
         });
-    }
+    }}
 
     /**
      * Reverse the migrations.
